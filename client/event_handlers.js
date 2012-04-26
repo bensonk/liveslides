bf01a527fa7beab2ce27177a4f02071724d4fac9
Ashleigh Baumgardner <LABaumgardner@gmail.com>
2012-04-26 13:30:39 -0700

    to change the current slide you must explicitly click the star. This makes it easier to just edit things without changing the current slide

--- client/event_handlers.js
+++ client/event_handlers.js
@@ -1,6 +1,7 @@
 Template.slide_list.events = {
-  'click #index ul li': function(e) { set_current_slide(e.currentTarget.id); },
+  'click #index ul li': function(e) { Session.set("client_current", e.currentTarget.id); },
   'click #new-slide':  function(e) { insert_slide(e); },
+  'click #index ul li .star': function(e) { set_current_slide($(e.currentTarget).parent().attr('id'));},
   'click a.admin-next': function(e) { move_next($(e.currentTarget).parents("li").attr("id")); },
   'click a.admin-prev': function(e) { move_prev($(e.currentTarget).parents("li").attr("id")); },
   'click a.admin-delete': function(e) { remove_slide($(e.currentTarget).parents("li").attr("id")); }
